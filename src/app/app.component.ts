60b4d00644a8d03b74f3b2814b35adbb1d1d5e2b
hoaC0 <hoachau1010@gmail.com>
2025-07-28 22:32:41 +0200

    Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from "./search/search.component";
+import { ListingComponent } from "./listing/listing.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, SearchComponent],
+  imports: [HeaderComponent, SearchComponent, ListingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/listing/listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HousingLocationInfo } from '../housing-location-info';
+
+@Component({
+  selector: 'app-listing',
+  imports: [],
+  templateUrl: './listing.component.html',
+  styleUrl: './listing.component.css'
+})
+export class ListingComponent {
+
+}
