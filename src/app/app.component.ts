6986762faf46cd231aa3b2d0dded926680e2ba1b
hoaC0 <hoachau1010@gmail.com>
2025-07-28 22:03:44 +0200

    Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from "./search/search.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent],
+  imports: [HeaderComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
